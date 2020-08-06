7a1835cc1ccbf85c5e387f4c50dfd7abfeaafcb7
FloweryK <flowerk94@gmail.com>
2020-08-06 10:17:45 +0900

    update on w2v

--- 99_word2vec/model.py
+++ 99_word2vec/model.py
@@ -1,24 +1,41 @@
+import torch
 import torch.nn as nn
 
 
 class CBOW(nn.Module):
-    def __init__(self, vocab_size, N_EMBED, N_HIDDEN):
+    def __init__(self, vocab_size, N_EMBED):
         super(CBOW, self).__init__()
 
         # embedding layer (B, 2*window) -> (B, 2*window, N_EMBED)
         self.embedding = nn.Embedding(vocab_size, N_EMBED)
 
-        # out layer (B, 2*window, N_HIDDEN) -> (B, 2*window, vocab_size)
-        self.linear2 = nn.Linear(N_EMBED, vocab_size)
+        # out layer (B, 2*window, N_EMBED) -> (B, 2*window, vocab_size)
+        self.linear = nn.Linear(N_EMBED, vocab_size)
         self.softmax = nn.LogSoftmax(dim=-1)
 
     def forward(self, x):
         # x: context (B, 2*window)
-        # label: (B, )
         x = self.embedding(x)       # (B, 2*window, N_EMBED)
         x = x.mean(dim=1)           # (B, N_EMBED)
-        # x = self.linear1(x)         # (B, N_HIDDEN)
-        # x = self.relu(x)            # (B, N_HIDDEN)
-        x = self.linear2(x)         # (B, vocab_size)
+        x = self.linear(x)          # (B, vocab_size)
         x = self.softmax(x)         # (B, vocab_size)
+        return x
+
+
+class CBOW_ngs(nn.Module):
+    def __init__(self, vocab_size, N_EMBED):
+        super(CBOW_ngs, self).__init__()
+
+        # embedding layer (B, 2*window) -> (B, 2*window, N_EMBED)
+        self.embedding = nn.Embedding(vocab_size, N_EMBED)
+
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x, y):
+        # x: context (B, 2*window)
+        # y: target (B, )
+        x = self.embedding(x)       # x: (B, 2*window, N_EMBED)
+        x = x.mean(dim=1)           # x: (B, N_EMBED)
+        y = self.embedding(y)       # y: (B, N_EMBED)
+
         return x
--- 99_word2vec/trainer.py
+++ 99_word2vec/trainer.py
@@ -1,9 +1,9 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 from dataset import PTBdata
-from model import CBOW
+from model import CBOW, CBOW_ngs
 
 
 def make_context_vector(context, word_to_ix):
@@ -11,29 +11,33 @@ def make_context_vector(context, word_to_ix):
     return torch.tensor(idxs)
 
 
-if __name__ == '__main__':
-    N_BATCH = 32
-    N_EMBED = 100
-    N_HIDDEN = 128
-    WINDOW = 2
+# SETTINGS
+N_BATCH = 32
+N_EMBED = 100
+WINDOW = 2
+N_EPOCH = 10000
 
+
+if __name__ == '__main__':
+    # dataset, dataloader
     trainset = PTBdata(path='ptb.train.txt',
-                       window=WINDOW,)
+                       window=WINDOW,
+                       limit=100)
     trainloader = DataLoader(dataset=trainset,
                              batch_size=N_BATCH,
                              shuffle=True)
 
+    # make vocab
     vocab_size = trainset.vocab_size
     word2idx = trainset.word2idx
     idx2word = trainset.idx2word
 
-    model = CBOW(vocab_size, N_EMBED, N_HIDDEN)
+    # model, loss, optimizer
+    model = CBOW(vocab_size, N_EMBED)
     criterion = nn.NLLLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.01)
 
-    total = len(trainloader)
-
-    for epoch in range(10000):
+    for epoch in range(N_EPOCH):
         running_loss = 0
         for i, data in enumerate(trainloader):
             targets = data['target']
@@ -49,7 +53,7 @@ if __name__ == '__main__':
             running_loss += loss.item()
 
             if i % 100 == 0:
-                print('%i %i %i %f %f' % (epoch, i, total, loss, running_loss))
+                print('epoch: %i | i: %i (/%i) | loss: %f (running loss: %f)' % (epoch, i, len(trainloader), loss, running_loss))
                 test = ['researchers', 'who', 'the', 'workers']
                 print(test)
                 test = make_context_vector(test, word2idx)
